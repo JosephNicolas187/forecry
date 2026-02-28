ace2298a7e8a249d03f9cc5d03d3891f7f1eeead
Yonohekel <josephairlangga125@gmail.com>
2026-03-01 00:31:35 +0700

    apalah

--- resources/views/prediksi.blade.php
+++ resources/views/prediksi.blade.php
@@ -125,7 +125,7 @@
                             <div class="d-grid">
                                 @auth
                                     <!-- Jika sudah login -->
-                                    <button type="submit" name="mode" value="prev" class="btn btn-outline-light flex-fill mb-2">
+                                    <button type="submit" name="mode" value="prev" class="btn btn-outline-light mb-2">
                                         Cek Akurasi (Close Sebelumnya)
                                     </button>
                                     <button type="submit" name="mode" value="next" class="btn btn-success btn-sm">
